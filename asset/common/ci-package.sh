419de6e3c5115144c8b19c29a389285ea230d765
易从 <781574155@qq.com>
2026-07-16 14:25:33 +0800

    p

--- asset/common/ci-package.sh
+++ asset/common/ci-package.sh
@@ -5,12 +5,15 @@ set -e
 PACKAGE_NAME=$1
 PACKAGE_VERSION=$2
 TARGET_PLATFORM=$3
+BRANCH_NAME=$4
 
-echo "Building package: $PACKAGE_NAME, version: $PACKAGE_VERSION, for platform: $TARGET_PLATFORM"
+echo "Building package: $PACKAGE_NAME, version: $PACKAGE_VERSION, for platform: $TARGET_PLATFORM, branch: $BRANCH_NAME"
 
+CACHE_BRANCH=$(echo "$BRANCH_NAME" | tr '/:' '--')
 docker buildx build --builder container-builder \
-	--cache-from=type=registry,ref=registry.openai36.com/tanqi/"$PACKAGE_NAME":buildcache \
-	--cache-to=type=registry,ref=registry.openai36.com/tanqi/"$PACKAGE_NAME":buildcache,mode=max \
+	--cache-from=type=registry,ref=registry.openai36.com/tanqi/"$PACKAGE_NAME":buildcache-master \
+	--cache-from=type=registry,ref=registry.openai36.com/tanqi/"$PACKAGE_NAME":buildcache-"$CACHE_BRANCH" \
+	--cache-to=type=registry,ref=registry.openai36.com/tanqi/"$PACKAGE_NAME":buildcache-"$CACHE_BRANCH",mode=max \
 	--push \
 	--platform "$TARGET_PLATFORM" \
 	-t registry.openai36.com/tanqi/"$PACKAGE_NAME":"$PACKAGE_VERSION" .
