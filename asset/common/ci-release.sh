3ba463233f88c40b6e13b58298b38fd22e0603d5
易从 <781574155@qq.com>
2026-07-18 19:17:46 +0800

    p

--- asset/common/ci-release.sh
+++ asset/common/ci-release.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+CI_SCRIPT_NAME=$(basename "$0")
+CI_SCRIPT_STARTED_SECONDS=$(date +%s)
+CI_SCRIPT_STARTED_AT=$(date '+%Y-%m-%d %H:%M:%S %z')
+echo "CI script ${CI_SCRIPT_NAME} started at: ${CI_SCRIPT_STARTED_AT}"
+trap 'CI_SCRIPT_EXIT_CODE=$?; CI_SCRIPT_ENDED_SECONDS=$(date +%s); CI_SCRIPT_ENDED_AT=$(date "+%Y-%m-%d %H:%M:%S %z"); CI_SCRIPT_ELAPSED_SECONDS=$((CI_SCRIPT_ENDED_SECONDS - CI_SCRIPT_STARTED_SECONDS)); echo "CI script ${CI_SCRIPT_NAME} ended at: ${CI_SCRIPT_ENDED_AT}"; echo "CI script ${CI_SCRIPT_NAME} elapsed seconds: ${CI_SCRIPT_ELAPSED_SECONDS}"; exit "${CI_SCRIPT_EXIT_CODE}"' EXIT
+
 TAG_NAME=$1
 DEPLOYMENT_REPO=$2
 PACKAGE_NAME=$3
--- asset/common/ci-test.sh
+++ asset/common/ci-test.sh
@@ -5,6 +5,12 @@
 
 set -e
 
+CI_SCRIPT_NAME=$(basename "$0")
+CI_SCRIPT_STARTED_SECONDS=$(date +%s)
+CI_SCRIPT_STARTED_AT=$(date '+%Y-%m-%d %H:%M:%S %z')
+echo "CI script ${CI_SCRIPT_NAME} started at: ${CI_SCRIPT_STARTED_AT}"
+trap 'CI_SCRIPT_EXIT_CODE=$?; CI_SCRIPT_ENDED_SECONDS=$(date +%s); CI_SCRIPT_ENDED_AT=$(date "+%Y-%m-%d %H:%M:%S %z"); CI_SCRIPT_ELAPSED_SECONDS=$((CI_SCRIPT_ENDED_SECONDS - CI_SCRIPT_STARTED_SECONDS)); echo "CI script ${CI_SCRIPT_NAME} ended at: ${CI_SCRIPT_ENDED_AT}"; echo "CI script ${CI_SCRIPT_NAME} elapsed seconds: ${CI_SCRIPT_ELAPSED_SECONDS}"; exit "${CI_SCRIPT_EXIT_CODE}"' EXIT
+
 E2E_TEST_BASE_URL=$1
 
 TEST_MODE=${2:-"--test"}
